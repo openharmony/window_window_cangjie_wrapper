b1d5659cd138213039f64cea3ac8e81fba775e80
yangzq233 <yangzhiqiang10@huawei.com>
2026-04-03 16:18:33 +0800

    适配llt用例编译
    Signed-off-by: yangzq233 <yangzhiqiang10@huawei.com>

--- test/display_test/test/entry/hvigorfile.ts
+++ test/display_test/test/entry/hvigorfile.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import { hapTasks } from '@ohos/cangjie-build-support';
+import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
     system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
